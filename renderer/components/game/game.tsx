6acfec29b7f38722e07566d8fea2c4413392cb14
Guilherme Oliveira <guilherme.oliveira.01@outlook.com>
2023-11-25 18:04:43 -0300

    Cria hook para componente e utiliza classe Game

--- renderer/components/game/game.tsx
+++ renderer/components/game/game.tsx
@@ -1,32 +1,17 @@
-import { useEffect, useRef } from "react"
+import React from "react"
+import { useGame } from "./use-game"
+import { Game as CGame } from "../../classes";
 
-export const Game = () => {
-  const canvasRef = useRef<HTMLCanvasElement>(null)
-
-  useEffect(() => {
-    if (canvasRef.current) {
-      const ctx = canvasRef.current.getContext('2d')
-
-      ctx.scale(1, 1)
-      ctx.imageSmoothingEnabled = false
-
-      ctx.fillStyle = 'black'
-      ctx.fillRect(0, 0, canvasRef.current.width, canvasRef.current.height);
-
-      ctx.fillStyle = 'white'
-      ctx.fillRect(0, 0, 10, 10)
-      ctx.fillRect(790, 590, 10, 10)
-      ctx.fillRect(0, 590, 10, 10)
-      ctx.fillRect(790, 0, 10, 10)
-    }
-  }, [])
+export const Game = React.memo(function Game() {
+  const { canvasRef } = useGame()
 
   return (
     <canvas
       ref={canvasRef}
-      width={800}
-      height={600}
+      width={CGame.DIMENSION.width}
+      height={CGame.DIMENSION.height}
       style={{ border: '0px solid red', width: '100%', height: '100%' }}
     />
   )
 }
+)

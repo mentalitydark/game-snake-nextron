c397ba1e0088a999722b47d039c1377a2bd7d733
Guilherme Oliveira <guilherme.oliveira.01@outlook.com>
2023-12-07 20:12:17 -0300

    chore: adiciona prop refs e functions

--- renderer/contexts/game/game-types.ts
+++ renderer/contexts/game/game-types.ts
@@ -1,7 +1,14 @@
 import { MutableRefObject } from "react"
 
 export interface GameContextProps {
-  points: () => number
-  changePoints: (p: number) => void
-  pointsRef: MutableRefObject<HTMLSpanElement>
+  refs: {
+    pointsRef: MutableRefObject<HTMLSpanElement>
+    overlayTextPrimaryRef: MutableRefObject<HTMLSpanElement>
+    overlayTextSecondaryRef: MutableRefObject<HTMLSpanElement>
+  }
+  functions: {
+    changePoints: (p: number) => void
+    setOverlayTextPrimary: (v: string) => void
+    setOverlayTextSecondary: (v: string) => void
+  }
 }
